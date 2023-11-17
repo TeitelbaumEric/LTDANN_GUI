55021f88a269340afe7ccf56b1e64d3e27fe49fe
TeitelbaumEric <ericteitelbaum@email.arizona.edu>
2023-11-16 17:33:04 -0700

    New Screens
    Added screens with back buttons to display network health and visualize object location

--- MainWindow.py
+++ MainWindow.py
@@ -4,8 +4,12 @@ class MainScreen(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Set the window properties
+        self.setWindowTitle("LTDANN")
+        self.setGeometry(100, 100, 600, 400)  # Set the window size
+
         # Create buttons
-        self.button1 = QPushButton('Check Receiver Health', self)
+        self.button1 = QPushButton('Check Network Health', self)
         self.button2 = QPushButton('Visualize Object Localization', self)
 
         # Set up the layout
@@ -23,11 +27,46 @@ class MainScreen(QMainWindow):
         self.button2.clicked.connect(self.show_screen2)
 
     def show_screen1(self):
-        self.setCentralWidget(QLabel('Screen 1'))
+        self.setCentralWidget(Screen1(self))
 
     def show_screen2(self):
-        self.setCentralWidget(QLabel('Screen 2'))
+        self.setCentralWidget(Screen2(self))
 
+class Screen1(QWidget):
+    def __init__(self, main_window):
+        super().__init__()
+        self.main_window = main_window
+
+        label = QLabel('Check network health')
+
+        back_button = QPushButton('Back to Main Screen', self)
+        back_button.clicked.connect(self.back_to_main)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(back_button)
+        self.setLayout(layout)
+
+    def back_to_main(self):
+        self.main_window.setCentralWidget(MainScreen())
+
+class Screen2(QWidget):
+    def __init__(self, main_window):
+        super().__init__()
+        self.main_window = main_window
+
+        label = QLabel('Show object localization')
+
+        back_button = QPushButton('Back to Main Screen', self)
+        back_button.clicked.connect(self.back_to_main)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(back_button)
+        self.setLayout(layout)
+
+    def back_to_main(self):
+        self.main_window.setCentralWidget(MainScreen())
 
 if __name__ == "__main__":
     app = QApplication([])
