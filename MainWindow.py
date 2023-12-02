95ce5b2ed7f8cd4ca1c6bad8a6ef8a4fe1c7fc44
Joshua Owens <jowens721@arizona.edu>
2023-12-01 21:34:25 -0600

    placeholder text for object localization

--- MainWindow.py
+++ MainWindow.py
@@ -93,21 +93,58 @@ class Screen2(QWidget):
         super().__init__()
         self.main_window = main_window
 
-        label = QLabel('SIM GOES HERE')
+        label = QLabel('Visualize object localization')
 
-        back_button = QPushButton('Back', self)
+        back_button = QPushButton('Back to Main Screen', self)
         back_button.clicked.connect(self.back_to_main)
 
-        back_button.setMaximumWidth(100)
-        back_button.setMaximumHeight(30)
+        # List widget to display receiver locations and calculated transmitter location
+        self.receiver_list = QListWidget(self)
+
+        # Add items to the list widget
+        self.receiver1_item = QListWidgetItem('Receiver 1: 0, 0, 5')
+        self.receiver2_item = QListWidgetItem('Receiver 2: 0, 1000, 2')
+        self.receiver3_item = QListWidgetItem('Receiver 3: 1000, 0, 3')
+        self.receiver4_item = QListWidgetItem('Receiver 4: 1000, 1000, 1')
+        self.transmitter_calc_pos = QListWidgetItem('Calculated position: ???, ???, ???')
+        self.transmitter_actual_pos = QListWidgetItem('Actual position: 500, 400, 2')
+
+        # Set the connection status for each receiver
+        # self.receiver1_item.setForeground(Qt.green)
+        # self.receiver2_item.setForeground(Qt.green)
+        # self.receiver3_item.setForeground(Qt.green)
+        # self.receiver4_item.setForeground(Qt.green)
+        # self.transmitter_calc_pos.setForeground(Qt.green)
+        # self.transmitter_actual_pos.setForeground(Qt.green)
+
+        # Add items to the list
+        self.receiver_list.addItem(self.receiver1_item)
+        self.receiver_list.addItem(self.receiver2_item)
+        self.receiver_list.addItem(self.receiver3_item)
+        self.receiver_list.addItem(self.receiver4_item)
+        self.receiver_list.addItem(self.transmitter_calc_pos)
+        self.receiver_list.addItem(self.transmitter_actual_pos)
+
+        # Button to run/rerun TDOA calculation
+        tdoa_button = QPushButton('Run TDoA simulation', self)
+        tdoa_button.clicked.connect(self.run_tdoa_sim)
 
         layout = QVBoxLayout()
         layout.addWidget(label)
+        layout.addWidget(tdoa_button)
+        layout.addWidget(self.receiver_list)
         layout.addWidget(back_button)
         self.setLayout(layout)
 
     def back_to_main(self):
         self.main_window.setCentralWidget(MainScreen())
+
+    def run_tdoa_sim(self):
+        # Change all receivers to connected and strong connection
+        self.transmitter_calc_pos.setText('Calculated position: 497.7, 398.5, 2.0')
+        self.transmitter_actual_pos.setText('Actual position: 500, 400, 2')
+        # for item in [self.receiver1_item, self.receiver2_item, self.receiver3_item, self.receiver4_item]:
+        #     item.setForeground(Qt.green)  # Set text color to green for connected receivers
 
 if __name__ == "__main__":
     app = QApplication([])
