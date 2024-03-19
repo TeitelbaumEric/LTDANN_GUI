1e031668424147a852fd37c93cd2528dd6a248e5
TeitelbaumEric <ericteitelbaum@email.arizona.edu>
2024-03-19 09:44:01 -0700

    Serial Communication
    Added serial communication to the gui. Everything sent over the serial is displayed in a text edit

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,8 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QLabel, QListWidget, QListWidgetItem, QWidget
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QLabel, QListWidget, QListWidgetItem, QWidget, QTextEdit
+from PySide6.QtCore import Qt, Signal, QThread
 from tdoa_sim.TDoA import *
 from random import randint
+import serial
 
 class MainScreen(QMainWindow):
     def __init__(self):
@@ -90,6 +91,20 @@ class Screen1(QWidget):
         for item in [self.receiver1_item, self.receiver2_item, self.receiver3_item, self.receiver4_item]:
             item.setForeground(Qt.green)  # Set text color to green for connected receivers
 
+class SerialWorker(QThread):
+    new_data = Signal(str)
+
+    def __init__(self, port_name, baud_rate):
+        super().__init__()
+        self.port_name = port_name
+        self.baud_rate = baud_rate
+
+    def run(self):
+        with serial.Serial(self.port_name, self.baud_rate) as ser:
+            while True:
+                line = ser.readline().decode().strip()
+                self.new_data.emit(line)
+
 class Screen2(QWidget):
     def __init__(self, main_window):
         super().__init__()
@@ -100,57 +115,26 @@ class Screen2(QWidget):
         back_button = QPushButton('Back to Main Screen', self)
         back_button.clicked.connect(self.back_to_main)
 
-        # List widget to display receiver locations and calculated transmitter location
-        self.receiver_list = QListWidget(self)
-
-        # Add items to the list widget
-        self.receiver1_item = QListWidgetItem('Receiver 1: 0, 0, 5')
-        self.receiver2_item = QListWidgetItem('Receiver 2: 0, 1000, 2')
-        self.receiver3_item = QListWidgetItem('Receiver 3: 1000, 0, 3')
-        self.receiver4_item = QListWidgetItem('Receiver 4: 1000, 1000, 1')
-        self.transmitter_calc_pos = QListWidgetItem('Calculated position: ???, ???, ???')
-        self.transmitter_actual_pos = QListWidgetItem('Actual position: 500, 400, 2')
-
-        # Add items to the list
-        self.receiver_list.addItem(self.receiver1_item)
-        self.receiver_list.addItem(self.receiver2_item)
-        self.receiver_list.addItem(self.receiver3_item)
-        self.receiver_list.addItem(self.receiver4_item)
-        self.receiver_list.addItem(self.transmitter_calc_pos)
-        self.receiver_list.addItem(self.transmitter_actual_pos)
-
-        # Button to run/rerun TDOA calculation
-        tdoa_button = QPushButton('Run TDoA simulation', self)
-        tdoa_button.clicked.connect(self.run_tdoa_sim)
+        # Text edit to display serial data
+        self.serial_text_edit = QTextEdit()
+        self.serial_text_edit.setReadOnly(True)
 
         layout = QVBoxLayout()
         layout.addWidget(label)
-        layout.addWidget(tdoa_button)
-        layout.addWidget(self.receiver_list)
+        layout.addWidget(self.serial_text_edit)
         layout.addWidget(back_button)
         self.setLayout(layout)
 
+        # Start serial communication
+        self.serial_worker = SerialWorker('ACM0', 115200)
+        self.serial_worker.new_data.connect(self.update_serial_text)
+        self.serial_worker.start()
+
+    def update_serial_text(self, data):
+        self.serial_text_edit.append(data)
+
     def back_to_main(self):
         self.main_window.setCentralWidget(MainScreen())
-
-    def run_tdoa_sim(self):
-        # run tdoa sim using current node locations
-        transmitter = Node(500, 400, 2)  # Transmitter
-        num_nanos = 31.25
-        receivers = [
-            Node(0, 0, 5),  # Receiver 1
-            Node(0, 1000, 2),  # Receiver 2
-            Node(1000, 0, 3),  # Receiver 3
-            Node(1000, 1000, 1)  # Receiver 4
-        ]      
-        calculated_pos = calculate_tdoa(transmitter, receivers, num_nanos, transmitter.z)
-        # Change all receivers to connected and strong connection
-        self.transmitter_calc_pos.setText(f'Calculated position: {round(calculated_pos[0],2)},' 
-                                          f'{round(calculated_pos[1],2)},' 
-                                          f'{round(calculated_pos[2],2)}')
-        self.transmitter_actual_pos.setText(f'Actual position: {500}, {400}, {2}')
-        # for item in [self.receiver1_item, self.receiver2_item, self.receiver3_item, self.receiver4_item]:
-        #     item.setForeground(Qt.green)  # Set text color to green for connected receivers
 
 if __name__ == "__main__":
     app = QApplication([])
