67831e781c5004830cbf431b329e702d4a246a89
carterphilipp1 <carterphilipp1@gmail.com>
2024-04-09 12:58:38 -0700

    added small cosmetic changes

--- main_screen.py
+++ main_screen.py
@@ -1,10 +1,8 @@
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QLabel, QPushButton
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QLabel, QPushButton, QHBoxLayout
 from PySide6.QtGui import QFont
-from PySide6.QtCore import Qt
-from serial import SerialException
-import time
+from PySide6.QtCore import Qt, QTimer
+from serial_manager import serial_manager
 from health_screen import NetworkHealth
-from serial_manager import SerialWorker
 from localization_screen import Twod_visualization_LS
 
 class MainScreen(QMainWindow):
@@ -24,7 +22,7 @@ class MainScreen(QMainWindow):
         main_layout.setContentsMargins(20, 20, 20, 20)
         main_layout.setSpacing(40)  # Increase spacing between buttons
 
-        title_label = QLabel("LTDANN")
+        title_label = QLabel("Localized Temporarily Deployable Autonomous Navigation Network (LTDANN)")
         title_label.setAlignment(Qt.AlignCenter)
         title_label.setFont(QFont("Arial", 24, QFont.Bold))
 
@@ -36,12 +34,30 @@ class MainScreen(QMainWindow):
         self.button3.clicked.connect(self.show_screentwoDimensional)
         self.button3.setFixedHeight(120)  # Increase button height
 
+        # Stylized component to display the number of communicating nodes
+        self.node_status_layout = QHBoxLayout()
+        self.node_status_label = QLabel("Communicating Nodes:")
+        self.node_status_label.setFont(QFont("Arial", 16))
+        self.node_status_value = QLabel("0")
+        self.node_status_value.setFont(QFont("Arial", 16, QFont.Bold))
+        self.node_status_value.setStyleSheet("color: green")
+        self.node_status_layout.addWidget(self.node_status_label)
+        self.node_status_layout.addWidget(self.node_status_value)
+        self.node_status_layout.setAlignment(Qt.AlignCenter)
+
         main_layout.addWidget(title_label)
+        main_layout.addLayout(self.node_status_layout)
         main_layout.addWidget(self.button1)
         main_layout.addWidget(self.button3)
 
         self.network_health_screen = NetworkHealth(self, self.num_nodes, self.network_size)
         self.twod_screen = Twod_visualization_LS(self, self.num_nodes, self.network_size)
+
+        # Update the number of communicating nodes every 1 second
+        self.update_timer = QTimer(self)
+        self.update_timer.timeout.connect(self.update_communicating_nodes)
+        self.update_timer.start(1000)  # Update every 1000 milliseconds (1 second)
+
 
     def show_screen1(self):
         self.network_health_screen.showMaximized()
@@ -50,3 +66,11 @@ class MainScreen(QMainWindow):
     def show_screentwoDimensional(self):
         self.twod_screen.showMaximized()
         self.hide()
+
+    def update_communicating_nodes(self):
+        receivers_data = serial_manager.get_latest_data()
+        if receivers_data is not None:
+            num_communicating_nodes = len(receivers_data)
+            self.node_status_value.setText(str(num_communicating_nodes))
+        else:
+            self.node_status_value.setText("0")
